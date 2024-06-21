b727829e48ff475e3c4f0fa05e478a51963bc3b0
lukfre <balthier@tuta.io>
2024-06-21 10:49:30 +0200

    added new csv file

--- check_adversarial_sample.py
+++ check_adversarial_sample.py
@@ -15,7 +15,7 @@ def initialize_models():
     model_name_base = "MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli"
     model_name_large = "MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli"
     model_name_large_2 = "Joelzhang/deberta-v3-large-snli_mnli_fever_anli_R1_R2_R3-nli"
-    
+
     tokenizer_base = AutoTokenizer.from_pretrained(model_name_base)
     model_base = AutoModelForSequenceClassification.from_pretrained(model_name_base)
 
@@ -23,34 +23,46 @@ def initialize_models():
     model_large = AutoModelForSequenceClassification.from_pretrained(model_name_large)
 
     tokenizer_large_2 = AutoTokenizer.from_pretrained(model_name_large_2)
-    model_large_2 = AutoModelForSequenceClassification.from_pretrained(model_name_large_2)
+    model_large_2 = AutoModelForSequenceClassification.from_pretrained(
+        model_name_large_2
+    )
 
-
-    models = {"base": model_base.to(device),
-             "large": model_large.to(device),
-             "large2": model_large_2.to(device)}
-    tokenizers = {"base": tokenizer_base,
-             "large": tokenizer_large,
-             "large2": tokenizer_large_2}
+    models = {
+        "base": model_base.to(device),
+        "large": model_large.to(device),
+        "large2": model_large_2.to(device),
+    }
+    tokenizers = {
+        "base": tokenizer_base,
+        "large": tokenizer_large,
+        "large2": tokenizer_large_2,
+    }
 
 
 if __name__ == "__main__":
     initialize_models()
 
-    choosen_model = random.choice(list(models.keys()))
-    print(f"Choosen Model: {choosen_model}")
+    chosen_model = random.choice(list(models.keys()))
+    print(f"Chosen Model: {chosen_model}")
 
     premise = input("Insert premise:")
     hypothesis = input("Insert hypothesis:")
     label = input("Insert gold label (entailment, neutral, contraddiction):")
 
     while True:
-        model_input = tokenizers[choosen_model](premise, hypothesis, truncation=True, return_tensors="pt")
-        output = models[choosen_model](model_input["input_ids"].to(device))  # device = "cuda:0" or "cpu"
+        model_input = tokenizers[chosen_model](
+            premise, hypothesis, truncation=True, return_tensors="pt"
+        )
+        output = models[chosen_model](
+            model_input["input_ids"].to(device)
+        )  # device = "cuda:0" or "cpu"
         prediction = torch.softmax(output["logits"][0], -1).tolist()
         label_names = ["entailment", "neutral", "contradiction"]
-        prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
-        
+        prediction = {
+            name: round(float(pred) * 100, 1)
+            for pred, name in zip(prediction, label_names)
+        }
+
         print(f"Gold label: {label}")
         print(f"Predicted label: {max(prediction, key=prediction.get)}")
         print(f"Predicted Percentage: {prediction}")
